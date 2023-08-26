1ceb85d8269b4d374aacef14b35ec07c0851da84
Lordahamey <andrew4peace@gmail.com>
2023-08-25 17:38:50 -0700

    6 n 7

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,25 +1,21 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
-* leet - encodes a string into 1337
-* @s: string to encode
-* written by cod-emminex
-* Return: address of s
-*/
 
-char *leet(char *s)
-{
-	int i, j;
-	char a[] = "aAeEoOtTlL";
-	char b[] = "4433007711";
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
-	for (i = 0; *(s + i); i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			if (a[j] == s[i])
-				s[i] = b[j];
-		}
-	}
-	return (s);
-}
+#endif
+
+/* by cod-emminex */
